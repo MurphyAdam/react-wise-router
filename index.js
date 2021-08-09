3a11d20985720c35a707d4cfeb0063ff3772c326
Adam Clark <elm.majidi@gmail.com>
2021-08-09 16:23:58 +0100

    version 1.1.0

--- index.js
+++ index.js
@@ -11,14 +11,25 @@ var _reactRouterDom = require("react-router-dom");
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
 
+var _excluded = ["component", "isAuthenticated", "needsAuthentication", "needsAuthorisation", "routePermissions", "userPermissions", "redirectTo", "defaultRedirect", "fallback", "debug", "passRouteProps"];
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
 
 function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
 
 function WiseRouter(props) {
-  var isAuthenticated = props.isAuthenticated,
+  var component = props.component,
+      isAuthenticated = props.isAuthenticated,
       needsAuthentication = props.needsAuthentication,
       needsAuthorisation = props.needsAuthorisation,
       routePermissions = props.routePermissions,
@@ -29,7 +40,17 @@ function WiseRouter(props) {
       fallback = props.fallback,
       _props$debug = props.debug,
       debug = _props$debug === void 0 ? false : _props$debug,
-      rest = _objectWithoutProperties(props, ["isAuthenticated", "needsAuthentication", "needsAuthorisation", "routePermissions", "userPermissions", "redirectTo", "defaultRedirect", "fallback", "debug"]);
+      _props$passRouteProps = props.passRouteProps,
+      passRouteProps = _props$passRouteProps === void 0 ? false : _props$passRouteProps,
+      rest = _objectWithoutProperties(props, _excluded);
+
+  var routeProps = {};
+  var passRoutePropsBool = !!passRouteProps && Array.isArray(passRouteProps);
+  var Component = component;
+
+  if (! /*#__PURE__*/_react.default.isValidElement( /*#__PURE__*/_react.default.createElement(Component, null))) {
+    throw new Error('Passed component is not a valid React component.');
+  }
   /*
   we could implement fallback component and its props in two ways:
   the one just below, or with `return React.createElement(fallback.component, fallback.props)`
@@ -40,9 +61,9 @@ function WiseRouter(props) {
   var FallbackProps = {};
 
   if (!!fallback) {
-    var component = fallback.component,
+    var _component = fallback.component,
         _props = fallback.props;
-    FallbackComponent = component;
+    FallbackComponent = _component;
     FallbackProps = _props;
   }
   /*
@@ -58,6 +79,25 @@ function WiseRouter(props) {
     console.log(props);
   }
 
+  if (!!passRoutePropsBool) {
+    var _iterator = _createForOfIteratorHelper(passRouteProps),
+        _step;
+
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        var p = _step.value;
+
+        if (props.hasOwnProperty(p)) {
+          routeProps[p] = props[p];
+        }
+      }
+    } catch (err) {
+      _iterator.e(err);
+    } finally {
+      _iterator.f();
+    }
+  }
+
   var hasAllPermissions = function hasAllPermissions() {
     return routePermissions.every(function (p) {
       return userPermissions.includes(p);
@@ -65,13 +105,43 @@ function WiseRouter(props) {
   };
 
   if (!needsAuthentication) {
-    return /*#__PURE__*/_react.default.createElement(_reactRouterDom.Route, rest);
+    return /*#__PURE__*/_react.default.createElement(_reactRouterDom.Route, _extends({
+      render: function render() {
+        return /*#__PURE__*/_react.default.createElement(Component, routeProps);
+      }
+    }, rest));
   } else if (needsAuthentication) {
     if (isAuthenticated) {
       if (!needsAuthorisation) {
-        return /*#__PURE__*/_react.default.createElement(_reactRouterDom.Route, rest);
-      } else if (needsAuthorisation && hasAllPermissions()) {
-        return /*#__PURE__*/_react.default.createElement(_reactRouterDom.Route, rest);
+        return /*#__PURE__*/_react.default.createElement(_reactRouterDom.Route, _extends({
+          render: function render() {
+            return /*#__PURE__*/_react.default.createElement(Component, routeProps);
+          }
+        }, rest));
+      } else if (needsAuthorisation) {
+        if (hasAllPermissions()) {
+          return /*#__PURE__*/_react.default.createElement(_reactRouterDom.Route, _extends({
+            render: function render() {
+              return /*#__PURE__*/_react.default.createElement(Component, routeProps);
+            }
+          }, rest));
+        } else if (!!passRoutePropsBool && !redirectTo) {
+          // user wants the component to only be accessed if authenticated + 
+          // authorised users
+          // but they don't wanna redirect user to any route, but rather 
+          // let the component handle what needs to be done itself.
+          // this is achieved by setting the passRouteProps to an array 
+          // of values, and having redirectTo as a falsy value (by default)
+          return /*#__PURE__*/_react.default.createElement(_reactRouterDom.Route, _extends({
+            render: function render() {
+              return /*#__PURE__*/_react.default.createElement(Component, routeProps);
+            }
+          }, rest));
+        } else {
+          return /*#__PURE__*/_react.default.createElement(_reactRouterDom.Redirect, {
+            to: !!redirectTo ? redirectTo : defaultRedirect
+          });
+        }
       } else {
         if (!!fallback) {
           return /*#__PURE__*/_react.default.createElement(FallbackComponent, FallbackProps);
@@ -83,7 +153,18 @@ function WiseRouter(props) {
       }
     }
 
-    if (!!fallback) {
+    if (!!passRoutePropsBool && !redirectTo) {
+      // user wants the component to only be accessed if authenticated, 
+      // but they don't wanna redirect user to any route, but rather 
+      // let the component handle what needs to be done itself.
+      // this is achieved by setting the passRouteProps to an array 
+      // of values, and having redirectTo as a falsy value (by default)
+      return /*#__PURE__*/_react.default.createElement(_reactRouterDom.Route, _extends({
+        render: function render() {
+          return /*#__PURE__*/_react.default.createElement(Component, routeProps);
+        }
+      }, rest));
+    } else if (!!fallback) {
       return /*#__PURE__*/_react.default.createElement(FallbackComponent, FallbackProps);
     } else {
       return /*#__PURE__*/_react.default.createElement(_reactRouterDom.Redirect, {
@@ -102,6 +183,7 @@ function WiseRouter(props) {
 }
 
 WiseRouter.propTypes = {
+  component: _propTypes.default.elementType.isRequired,
   needsAuthentication: _propTypes.default.bool,
   needsAuthorisation: _propTypes.default.bool,
   isAuthenticated: _propTypes.default.bool,
@@ -111,5 +193,6 @@ WiseRouter.propTypes = {
   defaultRedirect: _propTypes.default.string,
   fallback: _propTypes.default.oneOfType([_propTypes.default.null, _propTypes.default.object]),
   debug: _propTypes.default.bool,
+  passRouteProps: _propTypes.default.oneOfType([_propTypes.default.null, _propTypes.default.bool, _propTypes.default.array]),
   rest: _propTypes.default.array
 };
